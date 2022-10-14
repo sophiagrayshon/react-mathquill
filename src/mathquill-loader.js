699cb0f7a9f1660d8f365d03085ea36b7195d02c
sophiagrayshon <44779296+sophiagrayshon@users.noreply.github.com>
2022-10-14 22:08:20 +0100

    Update mathquill-loader.js

--- src/mathquill-loader.js
+++ src/mathquill-loader.js
@@ -1,5 +1,5 @@
 // webpack loaders are used to configure mathquill as a commonjs module, see webpack.config.js
-const MathQuill = require('@mudkipme/mathquill@0.11.3/build/mathquill.js')
+const MathQuill = require('/npm/@mudkipme/mathquill@0.11.3/build/mathquill.js')
 const MQ = MathQuill.getInterface(2)
 
 export default MQ
